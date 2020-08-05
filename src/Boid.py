f322eb5eaa1a7d05f675170b311b512ada9ffbe6
Cédric Audy <cedric.audy08@gmail.com>
2020-08-05 02:17:16 -0400

    some code for rule 1, added point class for boid attribute

--- src/Boid.py
+++ src/Boid.py
@@ -1,14 +1,44 @@
 import random
+import math
+import numpy as np
 
 class Boid:
     # ===============================================================================================
-    def __init__(self, x):
-        self.x = x
-        self.y = 0
+    def __init__(self, x = 0, y = 0):
+        self.position = Point(x,y)
         self.speed = 1 #scalar value
-        self.velocity = [] #vector
+        self.velocity = np.array([]*3) #vector
         self.neighbours = [] # other boids
     # ===============================================================================================
-    def update(self):
-        self.x += 1
-        self.y += 1
+    # def update(self):
+        # self.x += 1
+        # self.y += 1
+
+class Point(object):
+    # ===============================================================================================
+    def __init__(self, x, y):
+        self.X = x
+        self.Y = y
+
+    # ===============================================================================================
+    def move(self, dx, dy):
+        self.X = self.X + dx
+        self.Y = self.Y + dy
+
+    # ===============================================================================================
+    def __str__(self):
+        return "Point(%s,%s)"%(self.X, self.Y)
+
+    # ===============================================================================================
+    def getX(self):
+        return self.X
+
+    # ===============================================================================================
+    def getY(self):
+        return self.Y
+
+    # ===============================================================================================
+    def distance(self, p):
+        dx = self.X - p.X
+        dy = self.Y - p.Y
+        return hypot(dx, dy)
--- src/Model.py
+++ src/Model.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from Boid import Boid
 
 class Model:
@@ -6,6 +8,28 @@ class Model:
         self.worldSettings = WorldSettings()
         self.boidSettings = BoidSettings()
         self.boids = [Boid(1)] * self.worldSettings.initialPopulation #boids objects
+    
+    # ===============================================================================================
+    @staticmethod
+    def rule1(boids, factor):
+        #cohesion
+        #c = (b1.position + b2.position + ... + bN.position) / N
+        centerOfMass = 0
+        for b in boids:
+            centerOfMass += b.position
+        
+        return (centerOfMass / len(boids) - 1) / factor
+
+    # ===============================================================================================
+    @staticmethod
+    def rule2(boids):
+        #separation
+        x = 1
+    # ===============================================================================================
+    @staticmethod
+    def rule3(boids):
+        #alignment
+        x = 1
 
 # ===============================================================================================
 class BoidSettings:
@@ -13,6 +37,7 @@ class BoidSettings:
     def __init__(self):
         self.sight = 50 #sight radius in <units>
         self.maxSpeed = 20 #maximum speed allowed
+        self.cohesionFactor = 100
 
 # ===============================================================================================
 class WorldSettings:
